34f5b4d3f3d51056ffae290c7ef827cd33d4cb58
“Lorie <lorie.rocher@gmail.com>
2020-10-01 15:21:35 +0200

    first draft of authentification

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
 import './App.css';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
 
-    </div>
-  );
+  state = {
+    user: this.props.user
+  }
+
+  setUser = user => {
+    this.setState({
+      user: user
+    });
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <Navbar user={this.state.user} setUser={this.setUser} />
+
+
+        <Route
+          exact
+          path='/signup'
+          render={props => <Signup setUser={this.setUser} {...props} />}
+        />
+        <Route
+          exact
+          path='/login'
+          render={props => <Login setUser={this.setUser} {...props} />}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
+
