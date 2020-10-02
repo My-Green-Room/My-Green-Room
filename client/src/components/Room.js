4d215a4fd3d093a190b44f92db8a6a70f8bfa8e3
Roxanne <roxannebrod@yahoo.com>
2020-10-02 15:36:52 +0200

    worked on crud function in plant.js

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -1,16 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Navbar from "./Navbar";
 import AddPlant from "./AddPlant";
 import EditPlant from "./EditPlant";
 import PlantDetails from "./PlantDetails";
-import './style.css'
+import "./style.css";
 
 class Room extends Component {
+  state = {
+    plantBtnId: "",
+    addForm: false,
+    editForm: false,
+  };
+
+  handleAddForm = (event) => {
+    this.setState({
+      plantBtnId: event.target.id,
+      addForm: true,
+    });
+  };
+
+  closeAddForm = () => {
+    this.setState({ addForm: false });
+  };
   render() {
     return (
       <div>
+        <Navbar />
         <div className="room-container">
           <div>
-            <AddPlant />
+            {this.state.addForm ? (
+              <AddPlant
+                plantCat={this.state.plantBtnId}
+                closeAddForm={this.closeAddForm}
+              />
+            ) : (
+              <></>
+            )}
             <EditPlant />
             <PlantDetails />
           </div>
@@ -23,19 +48,18 @@ class Room extends Component {
           </div>
 
           <div class="button-container">
-            <a href="#" class="plant-button" id="cat">herbs</a>
-            <a href="#" class="plant-button" id="cat">succulent</a>
-            <a href="#" class="plant-button" id="cat">flower</a>
-            <a href="#" class="plant-button" id="cat">home plant</a>
-            <a href="#" class="plant-button" id="cat">cactus</a>
+            <button id="herbs" onClick={this.handleAddForm}>
+              herbs
+            </button>
+            <button>succulent</button>
+            <button>flower</button>
+            <button>home plant</button>
+            <button id="cactus">cactus</button>
           </div>
-
-
         </div>
       </div>
-    )
+    );
   }
 }
-
 
 export default Room;
