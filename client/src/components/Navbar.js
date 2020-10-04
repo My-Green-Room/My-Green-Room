4f59c23b721b093bbaadaa9e0101c0b11fed8387
mail@studio7berlin.de <mailstudio7berlin.de@mailstuindesMBP.fritz.box>
2020-10-04 08:37:04 +0200

    prepare pull request

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
-import { Logout } from './Login';
+import { Login } from './Login';
 import './style.css'
 
 class Navbar extends Component {
   render() {
     return (
       <nav className="nav-container">
-        <a href="#" class="nav-button" id="cat">LOGIN</a>
-        <a href="#" class="nav-button" id="cat">SIGNUP</a>
-        <a href="#" class="nav-button" id="cat">LOGOUT</a>
+        <a href="/" class="nav-button" id="cat">LOGOUT</a>
+        <a href="/signup" class="nav-button" id="cat">SIGNUP</a>
+        <a href="login" class="nav-button" id="cat">LOGIN</a>
       </nav>
     )
   }
