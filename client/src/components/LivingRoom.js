9498169f42bdec1d37077b3f45734c1765ebf733
Roxanne <roxannebrod@yahoo.com>
2020-10-05 18:01:35 +0200

    update version

--- client/src/components/LivingRoom.js
+++ client/src/components/LivingRoom.js
@@ -15,8 +15,11 @@ export default class LivingRoom extends Component {
 
   render() {
     let allPlants = this.state.plants.map((plant, index) => (
-      <img className={`plant plant${index}`} src={plant.imgPath[0]} />
+      <a href="./PlantDetails">
+        <img className={`plant plant${index}`} src={plant.imgPath[0]} />
+      </a>
     ));
+
     console.log("this is allPlants", allPlants);
     console.log("this is the user inside the living room", this.props.user._id);
     return (
