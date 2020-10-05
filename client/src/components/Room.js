16e06d29756cbdc54c29d0646d6245d41b5fcf7c
Roxanne <roxannebrod@yahoo.com>
2020-10-05 15:30:51 +0200

    worked on nursery.sjon

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Redirect} from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import Navbar from "./Navbar";
 import AddPlant from "./AddPlant";
 import EditPlant from "./EditPlant";
@@ -7,8 +7,7 @@ import PlantDetails from "./PlantDetails";
 import "./style.css";
 import axios from "axios";
 
-import nursery from "../assets/nursery.json"
-
+import nursery from "../assets/nursery.json";
 
 class Room extends Component {
   state = {
@@ -20,30 +19,32 @@ class Room extends Component {
   };
 
   handleAddForm = (event) => {
-    let selectedPlantCat = nursery.find(plant=> {
-      if(plant.category == event.target.id) {
-        return plant
+    let selectedPlantCat = nursery.find((plant) => {
+      if (plant.category == event.target.id) {
+        return plant;
       }
-    })
+    });
     this.setState({
       plantBtnId: event.target.id,
       addForm: true,
-      selectedPlantCatDefault : selectedPlantCat
+      selectedPlantCatDefault: selectedPlantCat,
     });
+    console.log(this.state);
   };
 
-  submitNewPlant = event => {
-    event.preventDefault()
-    axios.post('/api/plants', this.state.selectedPlantCatDefault)
-      .then(response => {
-        console.log(response)
+  submitNewPlant = (event) => {
+    console.log("hello there");
+    event.preventDefault();
+    axios
+      .post("/api/plants", this.state.selectedPlantCatDefault)
+      .then((response) => {
+        console.log("please work", response);
         this.setState({
-    addForm: false,
-
-        })
-        return <Redirect to='/room' />
-      })
-  }
+          addForm: false,
+        });
+        return <Redirect to="/room" />;
+      });
+  };
 
   closeAddForm = () => {
     this.setState({ addForm: false });
@@ -63,7 +64,6 @@ class Room extends Component {
     });
   };
 
-
   closeEditForm = () => {
     this.setState({ editForm: false });
   };
@@ -73,22 +73,22 @@ class Room extends Component {
       <div>
         <Navbar />
         <div class="button-container">
-            <button id="herbs" onClick={this.handleAddForm}>
-              herbs
-            </button>
-            <button id="succulent" onClick={this.handleAddForm}>
-              succulent
-            </button>
-            <button id="flower" onClick={this.handleAddForm}>
-              flower
-            </button>
-            <button id="homeplant" onClick={this.handleAddForm}>
-              home plant
-            </button>
-            <button id="cactus" onClick={this.handleAddForm}>
-              cactus
-            </button>
-          </div>
+          <button id="herbs" onClick={this.handleAddForm}>
+            herbs
+          </button>
+          <button id="succulent" onClick={this.handleAddForm}>
+            succulent
+          </button>
+          <button id="flower" onClick={this.handleAddForm}>
+            flower
+          </button>
+          <button id="homeplant" onClick={this.handleAddForm}>
+            home plant
+          </button>
+          <button id="cactus" onClick={this.handleAddForm}>
+            cactus
+          </button>
+        </div>
         <div className="room-container">
           <div>
             {this.state.addForm ? (
@@ -101,22 +101,17 @@ class Room extends Component {
               <></>
             )}
             {this.state.editForm ? (
-              <EditPlant
-                closeEditForm={this.closeEditForm}
-              />
+              <EditPlant closeEditForm={this.closeEditForm} />
             ) : (
               <></>
             )}
             {this.state.plantDetailsForm ? (
-            <PlantDetails
-                closeEditForm={this.closeEditForm} />
+              <PlantDetails closeEditForm={this.closeEditForm} />
             ) : (
               <></>
             )}
           </div>
-
         </div>
-          
       </div>
     );
   }
