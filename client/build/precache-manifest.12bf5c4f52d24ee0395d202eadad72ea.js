35661514a64897c1cc2c90056e60ff71433c3cde
“Lorie <lorie.rocher@gmail.com>
2020-10-08 17:36:38 +0200

    deepest secret

---
+++ client/build/precache-manifest.12bf5c4f52d24ee0395d202eadad72ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ac6f28f22a7408dab3f3bfed5225d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4771c83f74a9d8d534e7",
+    "url": "/static/css/main.f271b858.chunk.css"
+  },
+  {
+    "revision": "ef3c90de192ae4873305",
+    "url": "/static/js/2.025c1102.chunk.js"
+  },
+  {
+    "revision": "c7f92eccde8e496c47f87d08777731a5",
+    "url": "/static/js/2.025c1102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4771c83f74a9d8d534e7",
+    "url": "/static/js/main.f642f9c1.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d0a9ff1390b9260f85e59fc485f14b77.js"
+  "/precache-manifest.12bf5c4f52d24ee0395d202eadad72ea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
